3b048d169cd9ab3a47ad3a38e3de1d6ecba4798f
MJahidH <m.jahid.h@hotmail.com>
2024-05-23 21:04:38 +0100

    data fetching functions

--- profile-predictor/src/app/prediction/[name]/page.tsx
+++ profile-predictor/src/app/prediction/[name]/page.tsx
@@ -1,7 +1,23 @@
+interface Params {
+  param: { name: string };
+}
+
+const getPredictedAge = async (name: string) => {
+  const res = await fetch(`https://api.nationalize.io?name=${name}`);
+  return res.json();
+};
+
+const getPredictedGender = async (name: string) => {
+  const res = await fetch(`https://api.genderize.io?name=${name}`);
+  return res.json();
+};
+
+const getPredictedNationality = async (name: string) => {
+  const res = await fetch(`https://api.nationalize.io?name=${name}`);
+  return res.json();
+};
+
+
 export default function Home({ params }: any) {
-  return (
-    <main>
-      {params.name}
-    </main>
-  )
+  return <main>{params.name}</main>;
 }
