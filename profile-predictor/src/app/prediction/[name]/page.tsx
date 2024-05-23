1e0db6d7163fec35932073ab434a5701b74ff38c
MJahidH <m.jahid.h@hotmail.com>
2024-05-23 21:19:45 +0100

    params interface corrected and then passed in as type for the params

--- profile-predictor/src/app/prediction/[name]/page.tsx
+++ profile-predictor/src/app/prediction/[name]/page.tsx
@@ -1,9 +1,8 @@
-interface Params {
-  param: { name: string };
-}
+import { promiseHooks } from "v8";
+
 
 const getPredictedAge = async (name: string) => {
-  const res = await fetch(`https://api.nationalize.io?name=${name}`);
+  const res = await fetch(`https://api.agify.io?name=${name}`);
   return res.json();
 };
 
@@ -18,6 +17,17 @@ const getPredictedNationality = async (name: string) => {
 };
 
 
-export default function Home({ params }: any) {
+interface Params {
+  params: { name: string };
+}
+
+export default async function Home({ params }: Params) {
+  const ageData = getPredictedAge(params.name)
+  const getGender = getPredictedGender(params.name)
+  const countryData = getPredictedNationality(params.name)
+
+  const [age,gender,country] = await Promise.all([ageData,getGender,countryData])
+  
+  
   return <main>{params.name}</main>;
 }
