12444b335e77ffcf7b7504092de544495362047c
MJahidH <m.jahid.h@hotmail.com>
2024-05-23 17:04:03 +0100

    when submit button has been pressed, it redirects the dynamic page, for now it displays what ever the user has typed in

--- profile-predictor/src/app/page.tsx
+++ profile-predictor/src/app/page.tsx
@@ -1,17 +1,25 @@
 "use client";
 
-import { useState } from "react";
+import { FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  
   const [inputValue, setInputValue] = useState("");
+
+  // push  function allowsus to redirect use to another route
+  const { push } = useRouter();
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    push(`/prediction/${inputValue}`);
+  };
 
   return (
     <div>
       <div>
         <h1>Enter Your Name</h1>
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           onChange={(e) => {
             setInputValue(e.target.value);
---
+++ profile-predictor/src/app/prediction/[name]/page.tsx
@@ -0,0 +1,7 @@
+export default function Home({ params }: any) {
+  return (
+    <main>
+      {params.name}
+    </main>
+  )
+}
