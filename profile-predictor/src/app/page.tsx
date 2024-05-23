44c2f6aeb18b59cccc61185f89e6c53fd244725a
MJahidH <m.jahid.h@hotmail.com>
2024-05-23 16:10:29 +0100

    when user types in the text input, this vlaue gets added to the state

--- profile-predictor/src/app/page.tsx
+++ profile-predictor/src/app/page.tsx
@@ -1,11 +1,25 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  
+  const [inputValue, setInputValue] = useState("");
+
   return (
     <div>
       <div>
         <h1>Enter Your Name</h1>
       </div>
       <form>
-        <input type="text" placeholder="Type your name.."/>
+        <input
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+          value={inputValue}
+          type="text"
+          placeholder="Type your name.."
+        />
         <button type="submit">Enter</button>
       </form>
     </div>
