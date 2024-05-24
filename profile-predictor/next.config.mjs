c33a5aac9bd2aee2e5a8fc4f176c01e3dee8adc7
MJahidH <m.jahid.h@hotmail.com>
2024-05-24 19:27:38 +0100

    next config file edited

--- profile-predictor/next.config.mjs
+++ profile-predictor/next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+
+// Importing the type for type-checking purposes
+import { NextConfig } from 'next';
+
+/** @type {NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  target: "experimental-serverless-trace", // Note: Only use this line if absolutely necessary and compatible with your Next.js version
+};
 
 export default nextConfig;
